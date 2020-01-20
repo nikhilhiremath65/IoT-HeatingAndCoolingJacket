42fd9cc1dc4dfed7983ca5006fd3e3215508cc92
nikhilhiremath65 <60074997+nikhilhiremath65@users.noreply.github.com>
2020-01-20 13:42:22 -0700

    Heating and Cooling Jacket

---
+++ Code/Android/jacket/app/src/main/java/com/example/jacket/MyAppWebViewClient.java
@@ -0,0 +1,18 @@
+package com.example.jacket;
+import android.content.Intent;
+import android.net.Uri;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class MyAppWebViewClient extends WebViewClient {
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url){
+        if (Uri.parse(url).getHost().endsWith("instazop.com")){
+            return false;
+        }
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        view.getContext().startActivity(intent);
+        return true;
+    }
+}
